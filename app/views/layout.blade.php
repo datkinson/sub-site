ecbfe1e7a31587650ee3b6c06a57c5c7aef15b33
Daniel Atkinson <hourd.tasa@gmail.com>
2014-07-20 16:17:52 +0000

    updated template to bootstrap3 and change dmarkup of login form to match bootstrap

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,29 +2,50 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-    <title>Laravel Authentication Demo</title>
-    {{ HTML::style('/css/style.css') }}
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Multi-site Generator</title>
+	{{ HTML::style('/bootstrap/dist/css/bootstrap.min.css') }}
+	{{ HTML::style('/css/style.css') }}
 </head>
 <body>
-    <div id="container">
-        <div id="nav">
-            <ul>
-                <li><a href='/'>Home</a></li>
-                @if(Auth::check())
-                    <li><a href='/profile'>Profile</a></li>
-                    <li><a href='/logout'>Logout</a></li>
-                @else
-                    <li><a href='/login'>Login</a></li>
-                @endif
-            </ul>
-        </div><!-- end nav -->
+	<div id="container">
+		<div class="navbar navbar-default navbar-static-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Multi-Site</a>
+				</div>
 
-        <!-- check for flash notification message -->
-        @if(Session::has('flash_notice'))
-            <div id="flash_notice">{{ Session::get('flash_notice') }}</div>
-        @endif
+				<div id="nav" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li><a href='/'>Home</a></li>
+						@if(Auth::check())
+							<li><a href='/profile'>Profile</a></li>
+							<li><a href='/logout'>Logout</a></li>
+						@else
+							<li><a href='/login'>Login</a></li>
+						@endif
+					</ul>
+				</div><!-- end nav -->
+			</div>
+		</div>
 
-        @yield('content')
-    </div><!-- end container -->
-</body>
-</html>
+		<!-- check for flash notification message -->
+		@if(Session::has('flash_notice'))
+			<div id="flash_notice">{{ Session::get('flash_notice') }}</div>
+		@endif
+
+		@yield('content')
+			</div><!-- end container -->
+			<div class="footer">
+				{{ HTML::script('bootstrap/dist/js/bootstrap.min.js') }}
+			</div>
+		</body>
+	</html>
--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -2,32 +2,35 @@
 @extends('layout')
 
 @section('content')
+<div class="row">
+	<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12">
+		{{ Form::open(array('url' => 'login')) }}
+			<h1>Login</h1>
+			<!-- check for login error flash var -->
+			@if (Session::has('flash_error'))
+	 			<div id="flash_error">{{ Session::get('flash_error') }}</div>
+			@endif
 
-	{{ Form::open(array('url' => 'login')) }}
-		<h1>Login</h1>
-		<!-- check for login error flash var -->
-		@if (Session::has('flash_error'))
- 			<div id="flash_error">{{ Session::get('flash_error') }}</div>
-		@endif
+			<!-- if there are login errors, show them here -->
+			<p>
+				{{ $errors->first('email') }}
+				{{ $errors->first('password') }}
+			</p>
 
-		<!-- if there are login errors, show them here -->
-		<p>
-			{{ $errors->first('email') }}
-			{{ $errors->first('password') }}
-		</p>
+			<div class="form-group">
+				{{ Form::label('email', 'Email Address') }}
+				{{ Form::text('email', Input::old('email'), array('placeholder' => 'awesome@awesome.com', 'class' => 'form-control')) }}
+			</div>
 
-		<p>
-			{{ Form::label('email', 'Email Address') }}
-			{{ Form::text('email', Input::old('email'), array('placeholder' => 'awesome@awesome.com')) }}
-		</p>
+			<div class="form-group">
+				{{ Form::label('password', 'Password') }}
+				{{ Form::password('password', array('class' => 'form-control')) }}
+			</div>
 
-		<p>
-			{{ Form::label('password', 'Password') }}
-			{{ Form::password('password') }}
-		</p>
-
-		<p>{{ Form::submit('Submit!') }}</p>
-	{{ Form::close() }}
+			<p>{{ Form::submit('Submit!', array('class' => 'btn btn-default')) }}</p>
+		{{ Form::close() }}
+	</div>
+</div>
 
 @stop
 
