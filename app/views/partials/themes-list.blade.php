fe73593b1c44c790dbff106338bd38dab79792a6
Daniel Atkinson <hourd.tasa@gmail.com>
2014-07-21 13:18:09 +0000

    settings page now has a theme selector form, but is yet to have the ability to update the setting in the database

--- app/views/partials/themes-list.blade.php
+++ app/views/partials/themes-list.blade.php
@@ -4,9 +4,17 @@
 			$files = scandir($directory);
 			$themes = array_shift($files);
 			$themes = array_shift($files);
+			$theme_list = array();
 			foreach ($files as $file)
 			{
-				echo (string)$file, "\n";
+				$theme_list[ucfirst($file)] = $file;
 			}
-		?>
-
+?>
+
+{{ Form::open(array('url' => 'settings/theme', 'role' => 'form')) }}
+	<div class="form-group">
+		{{ Form::label('theme', 'Please select a theme:') }}
+		{{ Form::select('themes', $theme_list, null, array('class' => 'form-control')) }}
+	</div>
+	{{ Form::submit('Submit', array('class' => 'btn btn-default')) }}
+{{ Form::close() }}
